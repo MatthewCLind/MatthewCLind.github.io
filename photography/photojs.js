0ed2002851e7e07785b31eeb7091fe998cb5f8b3
Matthew Lind <32720559+MatthewCLind@users.noreply.github.com>
2018-08-08 17:16:58 -0700

    added photography, notes, home

---
+++ photography/photojs.js
@@ -0,0 +1,27 @@
+ var currentPhoto = 0;
+ var numPhotos = 13;
+ var intervelometer;
+ var delay = 10000;
+ 
+ function firstLoad()
+ {
+	document.getElementById("photo").src = "photos/0.JPG";
+	intervelometer = setInterval(advancePhoto, delay);
+ }
+ 
+ function clickAdvance()
+ {
+	clearInterval(intervelometer);
+	advancePhoto();
+	intervelometer = setInterval(advancePhoto, delay);
+ }
+ 
+ function advancePhoto()
+ {
+	currentPhoto++;
+	if(currentPhoto >= numPhotos)
+	{
+		currentPhoto = 0;
+	}
+	document.getElementById("photo").src = "photos/" + currentPhoto + ".JPG";
+ }
